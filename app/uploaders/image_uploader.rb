2395096892131eb7cd4ac712c2c31ea18929b189
LeeAnne Ward <leeanneward@Dallass-iMac.local>
2016-05-23 08:28:17 -0500

    figaro 2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,8 +14,6 @@ if Rails.env.production?
 else
     storage :file
 end
-  
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -32,7 +30,7 @@ end
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [200, 300]
   #
   # def scale(width, height)
   #   # do something
