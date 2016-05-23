ee9ef092fd8f04c350e0b43e6616079060286580
LeeAnne Ward <leeanneward@Dallass-iMac.local>
2016-05-23 08:18:40 -0500

    figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIPTOQFVNZCJHPI2A',                        # required
-    :aws_secret_access_key  => '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'                        # required
+    :aws_access_key_id      => ENV["aws_access_key_id"],     # required
+    :aws_secret_access_key  => ENV["aws_secret_access_key"], # required
   }
-  config.fog_directory  = 'footscoutdemo'                     # required
+  config.fog_directory  = ENV["fog_directory"]               # required
 end
