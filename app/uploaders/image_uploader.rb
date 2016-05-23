f6128d7e344e97bcbadfb5111810dd9995e18c7e
LeeAnne Ward <leeanneward@Dallass-iMac.local>
2016-05-23 09:35:11 -0500

    resize image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,10 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
 
 if Rails.env.production?
     storage :fog
